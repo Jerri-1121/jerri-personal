3a8339d5f567bac52cdf02c77bf89bb30fc7db90
Jerri-1121 <gyaneswar.rao.j@gmail.com>
2024-07-24 23:54:12 +0530

    admin and user modules added in routing file

--- L-AND-T/src/app/app.routes.ts
+++ L-AND-T/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import path from 'path';
+import { LoginComponent } from './auth/components/login/login.component';
+import { SignupComponent } from './auth/components/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "login", component: LoginComponent},
+    {path: "signup", component: SignupComponent},
+    {path: "admin", loadChildren:()=>import("./modules/admin/admin.module").then(e => e.AdminModule)},
+    {path: "user", loadChildren:()=>import("./modules/user/user.module").then(e => e.UserModule)}
+];
