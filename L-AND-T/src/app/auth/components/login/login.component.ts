0cb576c32a4960521bda049bf9993e21404ad08e
Jerri-1121 <gyaneswar.rao.j@gmail.com>
2024-07-25 21:42:13 +0530

    login form and signup form

--- L-AND-T/src/app/auth/components/login/login.component.ts
+++ L-AND-T/src/app/auth/components/login/login.component.ts
@@ -1,15 +1,42 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AngularMaterialModule } from '../../../AngularMaterialModule';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     CommonModule,
+    RouterOutlet,
+    RouterModule,
+    AngularMaterialModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  hidePassword = true;
+  loginForm: FormGroup;
+
+  constructor(private fb:FormBuilder) { 
+    this.loginForm = this.fb.group({
+      email: [null,[Validators.required,Validators.email]],
+      password: [null,[Validators.required]],
+      
+
+  })
+}
+
+  togglePasswordVisibility(){
+    this.hidePassword = !this.hidePassword;
+  }
+  onSubmit(){
+    console.log(this.loginForm.value);
+  }
 
 }
--- L-AND-T/src/app/auth/components/signup/signup.component.ts
+++ L-AND-T/src/app/auth/components/signup/signup.component.ts
@@ -1,15 +1,41 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AngularMaterialModule } from '../../../AngularMaterialModule';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    AngularMaterialModule
   ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+
+  signupForm: FormGroup; 
+  hidePassword = true;
+
+  constructor(private fb: FormBuilder){
+
+    this.signupForm = this.fb.group({
+      name : [null,[Validators.required]],
+      email : [null,[Validators.required, Validators.email]],
+      password : [null,[Validators.required, Validators.minLength(6)]],
+      confirm_password : [null,[Validators.required]]
+    })
+  }
+
+  togglePasswordVisibility(){
+    this.hidePassword = !this.hidePassword;
+  }
+
+  onSubmit(){
+    console.log(this.signupForm.value)
+  }
+
 
 }
