55a8d6ca8608c09986c9c52333cc57d484ff575d
Jerri-1121 <gyaneswar.rao.j@gmail.com>
2024-07-25 10:23:50 +0530

    custom material module added

---
+++ L-AND-T/src/app/AngularMaterialModule.ts
@@ -0,0 +1,96 @@
+import { NgModule } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatRippleModule } from '@angular/material/core';
+import { MatDividerModule } from '@angular/material/divider';
+
+
+@NgModule({
+    exports: [
+        MatInputModule,
+        MatCardModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatListModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatCheckboxModule,
+        MatRadioModule,
+        MatChipsModule,
+        MatExpansionModule,
+        MatStepperModule,
+        MatTabsModule,
+        MatGridListModule,
+        MatMenuModule,
+        MatTooltipModule,
+        MatPaginatorModule,
+        MatProgressSpinnerModule,
+        MatDialogModule,
+        MatAutocompleteModule,
+        MatBadgeModule,
+        MatButtonToggleModule,
+        MatBottomSheetModule,
+        MatProgressBarModule,
+        MatSliderModule,
+        MatSlideToggleModule,
+        MatTreeModule,
+        MatSortModule,
+       // MatTableModule,
+        MatRippleModule,
+        MatBadgeModule,
+        MatChipsModule,
+        MatExpansionModule,
+        MatStepperModule,
+        MatTabsModule,
+        MatGridListModule,
+        MatMenuModule,
+        MatTooltipModule,
+        MatPaginatorModule,
+        MatProgressSpinnerModule,
+        MatDialogModule,
+        MatAutocompleteModule,
+        MatBadgeModule,
+        MatButtonToggleModule,
+        MatBottomSheetModule,
+        MatProgressBarModule,
+        MatSliderModule,
+        MatSlideToggleModule,
+    ]
+})
+export class AngularMaterialModule {
+
+}
